96442588fa2d411c6307cd9c509eff57aad8f964
Ibrahim Erdem <erdem_ibo@yahoo.com>
2022-10-29 14:33:44 -0400

    10/29 Added

---
+++ src/arraylist_linkedlist_vector/_02_StringArrayList.java
@@ -0,0 +1,55 @@
+package arraylist_linkedlist_vector;
+
+import java.util.ArrayList;
+
+public class _02_StringArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<String> cities = new ArrayList<>();
+
+        cities.add("Chicago");
+        cities.add("Berlin");
+        cities.add("Miami");
+
+        System.out.println(cities);
+        System.out.println("The size of the ArrayList = " + cities.size());
+
+        cities.set(2, "Evanston");
+        System.out.println(cities);
+
+                        /*
+            Create an ArrayList and store below cities in it
+            Chicago
+            Berlin
+            Miami
+
+            Print the size of the ArrayList
+            Print the ArrayList
+
+            EXPECTED:
+            3
+            [Chicago, Berlin, Miami]
+             */
+
+
+        cities.remove("Berlin");
+        System.out.println(cities);
+        System.out.println(cities.size());
+
+        System.out.println(cities.remove(0));
+
+        System.out.println(cities);
+        System.out.println(cities.size());
+
+
+        //Task - 4
+        cities.add("New York");
+        cities.add("Rome");
+        cities.add("Gent");
+
+        System.out.println(cities);
+        System.out.println(cities.size());
+
+
+    }
+}
