130fa60707df693c945cd5c1a8a6107dff4c0514
Ibrahim Erdem <erdem_ibo@yahoo.com>
2022-10-29 00:18:33 -0400

    Project_05 added

--- src/projects/Project05.java
+++ src/projects/Project05.java
@@ -20,77 +20,160 @@ public class Project05 {
         findSecondGreatestAndSmallestWithSort(numbers3);
 
         System.out.println("\n---------------TASK 4 --------------\n");
+        int[] numbers4 = {10, 5, 6, 7, 8, 5, 15, 15};
+        findSecondGreatestAndSmallest(numbers4);
+
+        System.out.println("\n---------------TASK 5 --------------\n");
+
+        String[] str = {"foo", "bar", "Foo", "bar", "6", "abc", "6", "xyz"};
+        findDuplicatedElementsInAnArray(str);
+
+        System.out.println("\n---------------TASK 6 --------------\n");
+
+        String[] stationary = {"pen", "eraser", "pencil", "pen", "123", "abc", "pen", "eraser"};
+        findMostRepeatedElementInAnArray(stationary);
 
 
     }
 
 
-           public static void findGreatestAndSmallestWithSort(int[] numbers){
+                    //TASK-1
+    public static void findGreatestAndSmallestWithSort(int[] numbers) {
 
-                 Arrays.sort(numbers);
-               if (numbers.length > 0){
-                   System.out.println("Max = " + numbers[numbers.length - 1]);
-                   System.out.println("Min = " + numbers[0]);
-               }else{
-                   System.out.println("Array is empty!");
-               }
+        Arrays.sort(numbers);
+        if (numbers.length > 0) {
+            System.out.println("Max = " + numbers[numbers.length - 1]);
+            System.out.println("Min = " + numbers[0]);
+        } else {
+            System.out.println("Array is empty!");
+        }
 
-          }
+    }
+
+                    //TASK-2
+    public static void findGreatestAndSmallest(int[] numbers) {
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int number : numbers) {
+
+            if (number > max) {
+                max = number;
+            }
+
+            if (number < min) {
+                min = number;
+            }
+        }
+
+        if (numbers.length > 0) {
+            System.out.println("Max is = " + max);
+            System.out.println("Min is = " + min);
+        } else {
+            System.out.println("Array is empty!");
+        }
+
+    }
+
+                    //TASK-3
+    public static void findSecondGreatestAndSmallestWithSort(int[] numbers) {
+
+        Arrays.sort(numbers);
+        if (numbers.length > 0) {
+            System.out.println("Max = " + numbers[numbers.length - 2]);
+            System.out.println("Min = " + numbers[1]);
+        } else {
+            System.out.println("Array is empty!");
+        }
+    }
 
 
-          public static void findGreatestAndSmallest (int[] numbers){
+                    //TASK-4
+    public static int findMax(int[] numbers) {
+        int max = Integer.MIN_VALUE;
 
-              int max = Integer.MIN_VALUE;
-              int min = Integer.MAX_VALUE;
+        for (int n : numbers) {
+            if (n > max) max = n;
+        }
+        return max;
+    }
 
-              for (int number : numbers) {
+    public static int findMin(int[] numbers) {
+        int min = Integer.MAX_VALUE;
 
-                  if(number > max){
-                      max = number;
-                  }
+        for (int n : numbers) {
+            if (n < min) min = n;
+        }
 
-                  if(number < min){
-                      min = number;
-                  }
-              }
+        return min;
+    }
 
-              if (numbers.length > 0){
-                  System.out.println("Max is = " + max);
-                  System.out.println("Min is = " + min);
-              }else{
-                  System.out.println("Array is empty!");
-              }
+    public static void findSecondGreatestAndSmallest(int[] numbers) {
+        int secondMax = Integer.MIN_VALUE;
+        int secondMin = Integer.MAX_VALUE;
 
-          }
+        for (int n : numbers) {
+            if (n > secondMax && n < findMax(numbers)) {
+                secondMax = n;
+            }
 
-            public static void findSecondGreatestAndSmallestWithSort(int[] numbers){
+            if (n < secondMin && n > findMin(numbers)) {
+                secondMin = n;
+            }
+        }
 
-            Arrays.sort(numbers);
-                if (numbers.length > 0){
-                    System.out.println("Max = " + numbers[numbers.length - 2]);
-                    System.out.println("Min = " + numbers[1]);
-                }else{
-                    System.out.println("Array is empty!");
+        System.out.println("second max = " + secondMax);
+        System.out.println("second min = " + secondMin);
+    }
+
+                    //TASK-5
+
+    public static void findDuplicatedElementsInAnArray(String[] words) {
+
+        String duplicate = "";
+        int j;
+
+
+        for (int i = 0; i < words.length - 1; i++) {
+            for (j = i + 1; j < words.length; j++) {
+                if ((words[i].equals(words[j])) && (i != j)) {
+                    duplicate = words[j];
                 }
+            }
+        }
+        System.out.println(duplicate);
     }
 
-//                public static void findSecondGreatestAndSmallest(int[] numbers){
-//
-//                int secSmall = Integer.MIN_VALUE;
-//                int secGreat = Integer.MAX_VALUE;
-//
-//                    for (int number : numbers) {
-//
-//                        if(number > secGreat && )
-//                            secGreat = number;
-//                    }
-//
-//
-//                          Dogru gidiyor yarin devam _13_ de cevap var. 2.ekleme
-//
-//                 }
+                     //TASK-6
+
+    public static void findMostRepeatedElementInAnArray(String[] str) {
+
+        String mostRepeated = "";
+        int j;
 
 
+        for (int i = 0; i < str.length - 1; i++) {
+            for (j = i + 1; j < str.length; j++) {
+                if ((str[i].equals(str[j])) && (i != j)) {
+                    mostRepeated = str[j];
+                }
+            }
+        }
+        System.out.println(mostRepeated);
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
